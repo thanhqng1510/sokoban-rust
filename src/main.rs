7fb53de3b880d169731156dbe54053a974344755
Thanh Nguyen <thanhqng1510@gmail.com>
2021-05-30 23:53:45 +0700

    Change game_state.rs to game.rs as game_state is used for another resource

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ggez::{conf, event, GameResult};
 use specs::{World, WorldExt};
 
 use crate::components::register_components;
-use crate::game_state::Game;
+use crate::game::Game;
 use crate::levels::initialize_level;
 use crate::resources::register_resources;
 
@@ -12,13 +12,13 @@ mod components;
 mod entities;
 mod levels;
 mod systems;
-mod game_state;
+mod game;
 mod resources;
 
 
 fn main() -> GameResult {
     let context_builder = ggez::ContextBuilder::new("Rust Sokoban", "Thanh Nguyen - Ly Nguyen")
-        .window_setup(conf::WindowSetup::default().title("Rust Sokoban"))
+        .window_setup(conf::WindowSetup::default().title("Rust Sokoban").vsync(false))
         .window_mode(conf::WindowMode::default().dimensions(800., 600.))
         .add_resource_path(path::PathBuf::from("./resources"));
     let (context, event_loop) = &mut context_builder.build()?;
